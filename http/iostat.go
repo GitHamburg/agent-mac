a7493cef74f6e385c2d99767e877a7d6f65ea672
doublebean <yangdoudou@foundersc.com>
2016-09-22 13:30:13 +0800

    add Disk IO Stats for http.

--- http/iostat.go
+++ http/iostat.go
@@ -2,10 +2,11 @@ package http
 
 import (
 	"net/http"
+	"../funcs"
 )
 
 func configIoStatRoutes() {
 	http.HandleFunc("/page/diskio", func(w http.ResponseWriter, r *http.Request) {
-		//RenderDataJson(w, funcs.IOStatsForPage())
+		RenderDataJson(w, funcs.IOStatsForPage())
 	})
 }
